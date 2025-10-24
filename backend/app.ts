9d98350d2484b3a60272b207803d939cb1da37a7
shayanroychoudhury <shayan.roy31@gmail.com>
2025-10-25 01:55:45 +0530

    redis setup

--- backend/app.ts
+++ backend/app.ts
@@ -1,9 +1,34 @@
 import express from 'express'
 import { Server } from 'socket.io'
 import { createServer } from 'node:http'
+import { randomUUID } from "crypto";
+import { createClient } from 'redis'
+import dotenv from 'dotenv'
+dotenv.config()
+
+
 
 const app = express()
 const server = createServer(app) 
+
+const redisPort = process.env.REDIS_PORT as number | undefined
+const redisHost = process.env.REDIS_HOST 
+const redisPassword = process.env.REDIS_PASSWORD 
+
+const redisClient = createClient({
+  socket: {
+    host: redisHost,
+    port: redisPort,
+    tls: true,
+  },
+  password: redisPassword
+})
+
+redisClient.on('error', (err) => console.error('Redis Client Error', err))
+
+await redisClient.connect()
+
+
 const io = new Server(server, {
   cors: {
     origin: '*', 
@@ -22,6 +47,17 @@ io.on('connection', (socket) => {
   socket.on('disconnect', () => {
     console.log('user disconnected:', socket.id)
   })
+
+  
+  socket.on("create-room", async ({ userId }) => {
+    const roomId = randomUUID().slice(0, 6); 
+    await redisClient.hset(`room:${roomId}`, { host: userId, createdAt: Date.now() });
+    await redisClient.sadd(`room:${roomId}:users`, userId);
+
+    socket.join(roomId);
+    socket.emit("room-created", { roomId });
+    console.log(`Room created: ${roomId} by ${userId}`);
+  });
 })
 
 
