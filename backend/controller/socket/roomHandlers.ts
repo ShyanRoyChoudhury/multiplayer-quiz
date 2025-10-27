53c4c4a6668573f32fc9d50777151601c42006c4
shayanroychoudhury <shayan.roy31@gmail.com>
2025-10-27 11:46:45 +0530

    add network tolerance

--- backend/controller/socket/roomHandlers.ts
+++ backend/controller/socket/roomHandlers.ts
@@ -4,7 +4,7 @@ import { redisClient } from "../../redisClient.js";
 export function registerRoomHandlers(io: Server, socket: Socket) {
   console.log("user connected:", socket.id);
 
-  socket.on("join-room", async ({ roomId, username }) => {
+  socket.on("join-room", async ({ roomId, username }, callback) => {
     /* room join handler */
     try {
       console.log("join-room called:", roomId, username);
@@ -23,11 +23,9 @@ export function registerRoomHandlers(io: Server, socket: Socket) {
 
       // Add username to Redis set for this room
       await redisClient.sAdd(`room:${roomId}:users`, username);
-
+      await redisClient.expire(`room:${roomId}:users`, 600);
       // get updated user count
       const userCount = await redisClient.sCard(`room:${roomId}:users`);
-
-      // join socket.io room
       socket.join(roomId);
 
       // notify user 
@@ -35,6 +33,7 @@ export function registerRoomHandlers(io: Server, socket: Socket) {
       socket.to(roomId).emit("user-joined", { username });
 
       console.log(`User ${username} joined room ${roomId}. Users now: ${userCount}`);
+      return callback?.({ status: "ok" });
     } catch (err) {
       console.error("Error joining room:", err);
     }
@@ -59,6 +58,22 @@ export function registerRoomHandlers(io: Server, socket: Socket) {
       socket.to(roomId).emit("user-left", { username, userCount });
 
       console.log(`user ${username} left room ${roomId}. Remaining users: ${userCount}`);
+      if (userCount === 0) {
+        console.log(`No users left in room ${roomId}. Cleaning up room data...`);
+
+        const keysToDelete = [
+          `room:${roomId}`,
+          `room:${roomId}:users`,
+          `room:${roomId}:currentQuestion`,
+          `room:${roomId}:scores`,
+          `room:${roomId}:answered`,
+        ];
+
+        // Delete all room-related keys
+        await redisClient.del(keysToDelete);
+
+        console.log(`Cleaned up all data for room ${roomId}`);
+      }
     } catch (err) {
       console.error("Error handling disconnect:", err);
     }
--- quiz-fe/src/socket.ts
+++ quiz-fe/src/socket.ts
@@ -6,4 +6,10 @@ const SOCKET_URL = `${VITE_SOCKET_URL}`
 console.log("SOCKET_URL", SOCKET_URL)
 export const socket = io(SOCKET_URL, {
   autoConnect: false, // connect only when you need it
+  reconnection: true,
+  reconnectionAttempts: 10,
+  reconnectionDelay: 1000,
+  reconnectionDelayMax: 5000,
+  timeout: 10000,
+  transports: ["websocket"],
 });
