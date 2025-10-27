91facb28e094ba60e7350f4eda2d13813bfb28f8
shayanroychoudhury <shayan.roy31@gmail.com>
2025-10-27 13:06:15 +0530

    set base url for render

--- quiz-fe/src/App.tsx
+++ quiz-fe/src/App.tsx
@@ -9,22 +9,6 @@ import { Toaster } from "@/components/ui/sonner"
 
 function App() {
 
-  // useEffect(() => {
-  //   socket.on('connect', () => {
-  //     console.log('connected to socket server:', socket.id)
-  //   })
-
-  //   socket.on('disconnect', () => {
-  //     console.log('disconnected')
-  //   })
-
-  //   // cleanup
-  //   return () => {
-  //     socket.off('connect')
-  //     socket.off('disconnect')
-  //   }
-  // }, [])
-
   return (
     <>
       <div>
